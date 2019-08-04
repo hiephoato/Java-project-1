51ef6e069bf416116f46e5d8d799b851f5e4a09e
To Hoa Hiep <hiep.th0051@hsc.edu.vn>
2019-08-04 20:07:15 +0700

    Version F

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,6 +20,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, B");
+        System.out.println("Hello, C");
     }
 }
